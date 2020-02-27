45ff79ef28cc4be3e1228139ea7bfaa72ac39527
prevattc7976 <61566536+prevattc7976@users.noreply.github.com>
2020-02-27 18:34:27 -0500

    Add files via upload
    First Submission

---
+++ P2T1_SalesPrediction_PrevatteCorey.py
@@ -0,0 +1,16 @@
+#Total display of sales and profits
+#02/20/2020
+#CTI-110 P2T1-Sales Prediction
+#Corey Prevatte
+
+#Get the projected total sales.
+
+total_sales = float(input('Enter the projected sales: '))
+
+# Calculate the profit as 23 percent of total sales.
+profit = total_sales * 0.23
+
+#Display the profit.
+print('The profit is $', format(profit, ',.2f'))
+
+ 
